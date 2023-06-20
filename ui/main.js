c13b98b08cf1fe12da44396a30648e9cbb1779c2
Itamar Shabtai <itamar.shabtai.1@gmail.com>
2023-06-20 16:19:22 +0300

    Automatically copy data from Flash Player Standalone and the official launcher

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,90 @@
 const { app, BrowserWindow, Menu, MenuItem, shell } = require("electron")
-const {join} = require("path")
+const {join, sep, isAbsolute, resolve, dirname} = require("path")
 const express = require("express")
+const {existsSync, mkdirSync, readdirSync} = require("fs");
+const {copySync} = require("fs-extra/lib/copy");
+const {homedir} = require("os");
+
+function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
+    const initDir = isAbsolute(targetDir) ? sep : '';
+    const baseDir = isRelativeToScript ? __dirname : '.';
+
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = resolve(baseDir, parentDir, childDir);
+        try {
+            mkdirSync(curDir);
+        } catch (err) {
+            if (err.code === 'EEXIST') { // curDir already exists!
+                return curDir;
+            }
+
+            // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+            if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
+                throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+            }
+
+            const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+            if (!caughtErr || caughtErr && curDir === resolve(targetDir)) {
+                throw err; // Throw if it's just the last created dir.
+            }
+        }
+
+        return curDir;
+    }, initDir);
+}
+
+const writable_root_path = join(app.getPath("userData"), "Pepper Data/Shockwave Flash/WritableRoot");
+if (!existsSync(writable_root_path)) {
+    const external_apps_writable_root_paths = [];
+
+    // Try to copy data from Flash Player Standalone
+    let flash_player_dir;
+    if (process.platform === "linux") {
+        flash_player_dir = join(homedir(), ".macromedia/Flash_Player");
+    }
+    else {
+        flash_player_dir = join(process.env.APPDATA, "Macromedia/Flash Player");
+    }
+    external_apps_writable_root_paths.push(flash_player_dir, );
+
+    // Try to copy data from the official launcher
+    external_apps_writable_root_paths.push(writable_root_path.replace("evolved-dragonfable-launcher", "Artix Game Launcher"));
+
+    let user_pref_dir = null;
+    let object_dir_id = null;
+    for (const writable_root_dir of external_apps_writable_root_paths) {
+        const shared_objects_dir = join(writable_root_dir, "#SharedObjects");
+        if (!existsSync(shared_objects_dir)) {
+            continue;
+        }
+
+        let found = false;
+        for (const object_dir_name of readdirSync(shared_objects_dir)) {
+            for (const domain of ["play.dragonfable.com", "dragonlord.battleon.com", "dragonfable.battleon.com"]) {
+                const object_path = join(shared_objects_dir, object_dir_name, domain);
+                if (existsSync(object_path)) {
+                    object_dir_id = object_dir_name;
+                    user_pref_dir = object_path;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+
+    if (user_pref_dir && object_dir_id) {
+        let target_dir = join(writable_root_path, "#SharedObjects", object_dir_id, "127.0.0.1");
+        mkDirByPathSync(dirname(target_dir));
+        copySync(user_pref_dir, target_dir);
+    }
+}
+
 
 let pluginName;
 
