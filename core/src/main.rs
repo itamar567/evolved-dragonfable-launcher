1715ddcedb221340b1e0b7c3f68815ca2f0fa49b
Itamar <itamar.shabtai.1@gmail.com>
2023-07-15 17:43:02 +0300

    Revert "Kill the old server if it didn't close"
    This reverts commit 258a442a799d387a0ebb2c24be01a8aacade3422.

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,11 +1,10 @@
 #![feature(lazy_cell)]
 #![windows_subsystem = "windows"]
 
-use std::{fs, process, thread};
+#[cfg(not(debug_assertions))]
 use std::process::Command;
 use std::sync::LazyLock;
-use std::time::Duration;
-use crate::config::{LOCAL_SERVER_ADDR, PROJECT_DIRS};
+use crate::config::LOCAL_SERVER_ADDR;
 use axum::extract::Path;
 use axum::routing::{get, post};
 use axum::Router;
@@ -58,36 +57,14 @@ pub async fn main() {
         .fallback(get(server::get::unhandled_get_request).post(server::post::unhandled_post_request))
         .layer(TraceLayer::new_for_http());
 
-    let pid_file = &PROJECT_DIRS.cache_dir().join("pid");
 
-    if let Ok(Ok(old_process_id)) = fs::read_to_string(pid_file).map(|pid| pid.parse::<u32>()) {
-        #[cfg(target_os = "linux")] {
-            Command::new("kill")
-                .arg(old_process_id.to_string())
-                .spawn()
-                .unwrap()
-                .wait()
-                .unwrap();
-        }
-        #[cfg(target_os = "windows")] {
-            Command::new("taskkill")
-                .arg("/PID")
-                .arg(old_process_id.to_string())
-                .spawn()
-                .unwrap()
-                .wait()
-                .unwrap();
-        }
-
-        thread::sleep(Duration::from_millis(50));
-    }
-    fs::write(pid_file, process::id().to_string()).unwrap();
-
-    #[cfg(not(debug_assertions))] {
+    #[cfg(not(debug_assertions))]
+    {
         tokio::spawn(axum::Server::bind(&LOCAL_SERVER_ADDR.parse().unwrap()).serve(app.into_make_service()));
         Command::new("./ui").spawn().unwrap().wait().unwrap();
     }
-    #[cfg(debug_assertions)] {
+    #[cfg(debug_assertions)]
+    {
         axum::Server::bind(&LOCAL_SERVER_ADDR.parse().unwrap()).serve(app.into_make_service()).await.unwrap();
     }
 }
